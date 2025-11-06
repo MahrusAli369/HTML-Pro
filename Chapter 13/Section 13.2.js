720c64bbc66fd2864272d91f599a3144a9ed8d58
Mahrus Ali <maliknoornabi76@gmail.com>
2025-11-06 15:53:37 +0500

    Include JS code in HTML

---
+++ Chapter 13/Section 13.2.js
@@ -0,0 +1,3 @@
+// Section 13.2 - External JavaScript
+alert("Hello from External JavaScript File!");
+console.log("External JavaScript file is successfully linked.");
---
+++ Chapter 13/Section 13.4.js
@@ -0,0 +1,4 @@
+console.log("Async script loaded successfully!");
+console.log("This script runs independently of HTML parsing.");
+console.log("Async means it doesn’t wait for the page to finish loading before execution.");
+console.log("Useful for scripts that don't rely on DOM elements being present.");
